98a8f2940e51702e14d40deb8bfbd022d9f9f7c9
Vadim Kislov <kislov.v@gmail.com>
2022-03-31 15:25:38 +0300

    feat: add set view color after change sliders

--- ColorInstaller/ViewController.swift
+++ ColorInstaller/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var currentColorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currentColorView.layer.cornerRadius = 10
+
+        redSlider.value = getRandomColorHue()
+        greenSlider.value = getRandomColorHue()
+        blueSlider.value = getRandomColorHue()
+        
+        setColorLabelFor(sliders: redSlider, greenSlider, blueSlider)
+        
+        setViewColor()
     }
 
-
+    @IBAction func onChangeSlider(_ sender: UISlider) {
+        setViewColor()
+        
+        setColorLabelFor(sliders: sender)
+    }
+    
+    private func setViewColor() {
+        currentColorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setColorLabelFor(sliders: UISlider...) {
+        for slider in sliders {
+            switch slider {
+            case redSlider:
+                redValueLabel.text = sliderValueFormatter(redSlider.value)
+            case greenSlider:
+                greenValueLabel.text = sliderValueFormatter(greenSlider.value)
+            default:
+                blueValueLabel.text = sliderValueFormatter(blueSlider.value)
+            }
+        }
+    }
+    
+    private func getRandomColorHue() -> Float {
+        return round(Float.random(in: 0...1) * 100) / 100
+    }
+    
+    private func sliderValueFormatter(_ value: Float) -> String {
+        String(format: "%.2f", value)
+    }
 }
-
