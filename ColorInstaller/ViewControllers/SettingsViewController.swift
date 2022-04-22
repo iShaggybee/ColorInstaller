d314629255c95d75b15f3edacaabdde0a6b18835
Vadim Kislov <kislov.v@gmail.com>
2022-04-22 11:57:30 +0300

    feat: add text fields for set color

--- ColorInstaller/ViewControllers/SettingsViewController.swift
+++ ColorInstaller/ViewControllers/SettingsViewController.swift
@@ -18,6 +18,10 @@ class SettingsViewController: UIViewController {
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
     
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
     var backgroundColor: UIColor!
     
     var delegate: WelcomeViewControllerDelegate!
@@ -28,8 +32,10 @@ class SettingsViewController: UIViewController {
         currentColorView.layer.cornerRadius = 10
         
         (redSlider.value, greenSlider.value, blueSlider.value) = backgroundColor.rgb
-
-        setColorLabelFor(sliders: redSlider, greenSlider, blueSlider)
+        
+        setTextFields(redTextField, greenTextField, blueTextField)
+        
+        setLabelAndFieldFor(sliders: redSlider, greenSlider, blueSlider)
         
         setViewColor()
     }
@@ -37,7 +43,7 @@ class SettingsViewController: UIViewController {
     @IBAction func onChangeSlider(_ sender: UISlider) {
         setViewColor()
         
-        setColorLabelFor(sliders: sender)
+        setLabelAndFieldFor(sliders: sender)
     }
     
     @IBAction func onSaveColor() {
@@ -55,25 +61,62 @@ class SettingsViewController: UIViewController {
         )
     }
     
-    private func setColorLabelFor(sliders: UISlider...) {
+    private func setLabelAndFieldFor(sliders: UISlider...) {
         for slider in sliders {
             switch slider {
             case redSlider:
-                redValueLabel.text = sliderValueFormatter(redSlider.value)
+                redValueLabel.text = floatValueFormatter(redSlider.value)
+                redTextField.text = floatValueFormatter(redSlider.value)
             case greenSlider:
-                greenValueLabel.text = sliderValueFormatter(greenSlider.value)
+                greenValueLabel.text = floatValueFormatter(greenSlider.value)
+                greenTextField.text = floatValueFormatter(greenSlider.value)
             default:
-                blueValueLabel.text = sliderValueFormatter(blueSlider.value)
+                blueValueLabel.text = floatValueFormatter(blueSlider.value)
+                blueTextField.text = floatValueFormatter(blueSlider.value)
             }
         }
     }
     
-    private func getRandomColorHue() -> Float {
-        return round(Float.random(in: 0...1) * 100) / 100
+    private func setSliderAndLabelFor(field: UITextField) {
+        let text = field.text ?? "0"
+        
+        switch field {
+        case redTextField:
+            redSlider.value = Float(text) ?? 0
+            redValueLabel.text = floatValueFormatter(redSlider.value)
+        case greenTextField:
+            greenSlider.value = Float(text) ?? 0
+            greenValueLabel.text = floatValueFormatter(greenSlider.value)
+        default:
+            blueSlider.value = Float(text) ?? 0
+            blueValueLabel.text = floatValueFormatter(blueSlider.value)
+        }
+    }
+
+    private func floatValueFormatter(_ value: Float) -> String {
+        String(format: "%.2f", value)
     }
     
-    private func sliderValueFormatter(_ value: Float) -> String {
-        String(format: "%.2f", value)
+    private func setTextFields(_ textFields: UITextField...) {
+        for textField in textFields {
+            textField.delegate = self
+            setKeyboardToolbar(for: textField)
+        }
+    }
+    
+    private func setKeyboardToolbar(for field: UITextField) {
+        let bar = UIToolbar()
+        let leftSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(tapDone))
+        
+        bar.items = [leftSpace, doneButton]
+        bar.sizeToFit()
+
+        field.inputAccessoryView = bar
+    }
+    
+    @objc private func tapDone() {
+        view.endEditing(true)
     }
 }
 
@@ -83,8 +126,57 @@ extension UIColor {
         var green: CGFloat = 0
         var blue: CGFloat = 0
         var alpha: CGFloat = 0
+        
         getRed(&red, green: &green, blue: &blue, alpha: &alpha)
 
         return (Float(red), Float(green), Float(blue))
+    }
+}
+
+extension SettingsViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+
+        guard let stringRange = Range(range, in: currentText) else { return false }
+
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+
+        return updatedText.count <= 4
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let currentText = textField.text ?? ""
+        
+        if currentText.isEmpty {
+            showAlert(title: "Error", message: "Text cannot be empty")
+            return
+        }
+        
+        guard let value = Double(currentText) else {
+            showAlert(title: "Error", message: "Value must be numeric")
+            return
+        }
+        
+        if !(0...1.0).contains(value) {
+            showAlert(title: "Error", message: "Value must be between 0 and 1")
+            return
+        }
+        
+        setSliderAndLabelFor(field: textField)
+        
+        setViewColor()
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default)
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true)
     }
 }
